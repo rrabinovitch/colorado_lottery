bdd27694420b75c60f2c68d4f80dc9bf1ed0c34f
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-26 21:51:41 -0600

    Add national_drawing attribute and national_drawing? method

--- lib/game.rb
+++ lib/game.rb
@@ -3,5 +3,11 @@ class Game
   def initialize(name, cost, national_drawing = false)
     @name = name
     @cost = cost
+    @cost = cost
+    @national_drawing = national_drawing
+  end
+
+  def national_drawing?
+    @national_drawing
   end
 end
