f5ce29d76e116df5e3056982e43516103022fc0a
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-26 21:02:38 -0600

    Test that game class exists

--- test/game_test.rb
+++ test/game_test.rb
@@ -7,4 +7,9 @@ class GameTest < Minitest::Test
     @pick_4 = Game.new('Pick 4', 2)
     @mega_millions = Game.new('Mega Millions', 5, true)
   end
+
+  def test_it_exists
+    assert_instance_of Game, @pick_4
+    assert_instance_of Game, @mega_millions
+  end
 end
