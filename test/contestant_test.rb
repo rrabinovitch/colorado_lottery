e08373b12883387818d327701761b90021c45510
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-26 22:06:23 -0600

    Test for out of state

--- test/contestant_test.rb
+++ test/contestant_test.rb
@@ -21,4 +21,8 @@ class ContestantTest < Minitest::Test
     assert_equal "CO", @alexander.state_of_residence
     assert_equal 10, @alexander.spending_money
   end
+
+  def test_it_knows_if_its_out_of_state
+    assert_equal false, @alexander.out_of_state?
+  end
 end
