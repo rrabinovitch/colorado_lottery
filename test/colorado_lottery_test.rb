9587435e5a28a149e3e8a0da96430b75d7e5d5f3
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-26 22:21:07 -0600

    Create CO lottery test and class files; set up test file

---
+++ test/colorado_lottery_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/colorado_lottery'
+
+class ColoradoLotteryTest < Minitest::Test
+  def setup
+    @lottery = ColoradoLottery.new
+  end
+
+  def test_it_exists
+    assert_instance_of ColoradoLottery, @lottery
+  end
+end
