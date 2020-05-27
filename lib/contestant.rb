37ff23b60f8a64102516dd1820c0893b46635be6
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-26 22:02:31 -0600

    Define full_name attribute and attr_reader

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,5 +1,6 @@
 class Contestant
+  attr_reader :full_name
   def initialize(contestant_params)
-  
+    @full_name = contestant_params[:first_name] + " " + contestant_params[:last_name]
   end
 end
