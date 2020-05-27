5ffc8801401be818c95d8d8aec94c5aec3890616
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-26 22:04:56 -0600

    Create instance variables and attr_readers for remaining attributes

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,6 +1,9 @@
 class Contestant
-  attr_reader :full_name
+  attr_reader :full_name, :age, :state_of_residence, :spending_money
   def initialize(contestant_params)
     @full_name = contestant_params[:first_name] + " " + contestant_params[:last_name]
+    @age = contestant_params[:age]
+    @state_of_residence = contestant_params[:state_of_residence]
+    @spending_money = contestant_params[:spending_money]
   end
 end
